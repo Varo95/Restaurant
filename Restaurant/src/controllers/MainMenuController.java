3e3d43be4053eecfa56f271f2253c479515afb32
gbret <gbret@192.168.1.11>
2021-03-05 18:28:57 +0100

    Main menu controler casi listo

--- Restaurant/src/controllers/MainMenuController.java
+++ Restaurant/src/controllers/MainMenuController.java
@@ -22,79 +22,180 @@ public class MainMenuController implements IMainMenuController {
 		result=true;
 		return result;
 	}
+    @Override
+    public boolean changeOrder(Client c) {
+        boolean result=false;
+        if (c!=null) {
+        	
+        	for(int i=0;i<orders.getAllOrders().size();i++) {
+        		
+        		if(orders.getAllOrders().get(i).equals(c)) {
+        			
+        		orders.getAllOrders().get(i).setClient(c);
+        		result =true;
+        		}
+        	}
+        }
+        return result;
+    }
 
-	@Override
-	public boolean changeOrder(Client c) {
-		boolean result=false;
-		orders.getAllOrders().get(1).setClient(c);
-		return result;
-	}
+    @Override
+    public boolean changeOrder(LocalDateTime d) {
+        boolean result=false;
+        
+        if (d!=null) {
+        	
+        	for(int i=0;i<orders.getAllOrders().size();i++) {
+        		
+        		if(orders.getAllOrders().get(i).equals(d)) {
+        			
+        		orders.getAllOrders().get(i).setDate(d);
+        		result =true;
+        		}
+        	}
+        }
+        return result;
+    }
 
-	@Override
-	public boolean changeOrder(LocalDateTime d) {
-		boolean result=false;
-		return result;
-	}
+    @Override
+    public boolean changeOrder(Client c, LocalDateTime d) {
+        boolean result=false;
+        boolean resultC=false;
+        boolean resultD=false;
+        if(c!=null && d!= null) {
+        	//CLIENT
+        	for(int i=0;i<orders.getAllOrders().size();i++) {
+        		
+        		if(orders.getAllOrders().get(i).equals(c)) {
+        			
+        		orders.getAllOrders().get(i).setClient(c);
+        		resultC =true;
+        		}
+        	//DATE
+        	for(int j=0;j<orders.getAllOrders().size();j++) {
+        		
+        		if(orders.getAllOrders().get(j).equals(d)) {
+        			
+        		orders.getAllOrders().get(j).setDate(d);
+        		resultD =true;
+        		
+        	
+        		}
+        	}
+        	
+        	if(resultC ==true && resultD ==true) {
+        	
+        		result=true;
+        	}
+        	}
+        }
+        return result;
+    }
 
-	@Override
-	public boolean changeOrder(Client c, LocalDateTime d) {
-		boolean result=false;
-		return result;
-	}
+    @Override
+    public boolean deleteOrder(Client c) {
+        boolean result=false;
+        if(c!= null) {
+        for(int i=0; i<orders.getAllOrders().size();i++) {
+        	
+        	if(orders.getAllOrders().get(i).equals(c)) {
+        		
+        		orders.getAllOrders().remove(i);
+        		result=true;
+        	}
+        }
+        }
+        return result;
+    }
 
-	@Override
-	public boolean deleteOrder(Client c) {
-		boolean result=false;
-		return result;
-	}
+    @Override
+    public boolean deleteOrder(LocalDateTime d) {
+        boolean result=false;
+        if(d!= null) {
+        for(int i=0; i<orders.getAllOrders().size();i++) {
+        	
+        	if(orders.getAllOrders().get(i).equals(d)) {
+        		
+        		orders.getAllOrders().remove(i);
+        		result=true;
+        	}
+        }
+        }
+        return result;
+    }
 
-	@Override
-	public boolean deleteOrder(LocalDateTime d) {
-		boolean result=false;
-		return result;
-	}
+    @Override
+    public boolean deleteOrder(Client c, LocalDateTime d) {
+        boolean result=false;
+        boolean resultC=false;
+        boolean resultD=false;
+        if (c != null && d !=null) {
+        
+        	//CLIENT
+        	for(int i=0;i<orders.getAllOrders().size();i++) {
+        		
+        		if(orders.getAllOrders().get(i).equals(c)) {
+        			
+        		orders.getAllOrders().remove(i);
+        		resultC =true;
+        		}
+        	//DATE
+        	for(int j=0;j<orders.getAllOrders().size();j++) {
+        		
+        		if(orders.getAllOrders().get(j).equals(d)) {
+        			
+        		orders.getAllOrders().remove(j);
+        		resultD =true;
+        		
+        	
+        		}
+        		if(resultC ==true && resultD ==true) {
+                	
+            		result=true;
+            	}
+        	
+        	
+        }
+        	}
+        }
+        return result;
+    }
 
-	@Override
-	public boolean deleteOrder(Client c, LocalDateTime d) {
-		boolean result=false;
-		return result;
-	}
+    @Override
+    public double cashToDay() {
+        double result=-1;
+        return result;
+    }
 
-	@Override
-	public double cashToDay() {
-		double result=-1;
-		return result;
-	}
+    @Override
+    public double cashThisMonth() {
+        double result=-1;
+        return result;
+    }
 
-	@Override
-	public double cashThisMonth() {
-		double result=-1;
-		return result;
-	}
+    @Override
+    public double cashTotal() {
+        double result=-1;
+        return result;
+    }
 
-	@Override
-	public double cashTotal() {
-		double result=-1;
-		return result;
-	}
+    @Override
+    public ArrayList<Order> viewOrdersNotPayed() {
+        ArrayList<Order> result=null;
+        return result;
+    }
 
-	@Override
-	public ArrayList<Order> viewOrdersNotPayed() {
-		ArrayList<Order> result=null;
-		return result;
-	}
+    @Override
+    public ArrayList<Order> viewOrdersPendingDelivered() {
+        ArrayList<Order> result=null;
+        return result;
+    }
 
-	@Override
-	public ArrayList<Order> viewOrdersPendingDelivered() {
-		ArrayList<Order> result=null;
-		return result;
-	}
-
-	@Override
-	public void saveAllAndClose() {
-		
-	}
-	
-	
+    @Override
+    public void saveAllAndClose() {
+        
+    }
+    
+    
 
 }
